23dc6a7aaf18fca060fd0430f08eb76257697652
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:19:17 +0530

    Create 110_update_persons_address.js

---
+++ 110_update_persons_address.js
@@ -0,0 +1,13 @@
+/*
+Given a person's previous address in the prefilled code, and current address as inputs, write a method in the given JS factory function to update the person's address.
+Input
+The first line of input contains a string  currentCity
+The second line of input contains a string currentState
+Output
+The output should be a single line containing the person's current city and state separated by a space.
+Sample Input 
+Amritsar
+Punjab
+Sample Output 
+Amritsar Punjab
+*/
