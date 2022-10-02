b195fd9af0c90f194e4daa7d8658190bcfdfe4cd
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:28:48 +0530

    Create 9_create_function.js

---
+++ 9_create_function.js
@@ -0,0 +1,16 @@
+/*
+Create a Function
+Write a function 'getNationalBird' to return the national bird of India.
+The national bird of India is the "Peacock".
+Output
+
+The output should be a single line containing the national bird of the India.
+*/
+function getNationalBird()
+{
+    let bird = "Peacock";
+    return bird;
+}
+
+let nationalBird = getNationalBird();
+console.log(nationalBird);
