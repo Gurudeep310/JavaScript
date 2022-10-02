f07e816d2ab59db8be0ef533a72702c326428f12
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:21:12 +0530

    Create 14_create_and_log_object.js

---
+++ 14_create_and_log_object.js
@@ -0,0 +1,25 @@
+/*
+Create & Log an Object - 2
+Write a JS program to log a movie object with the given properties (key-value pairs).
+
+title - Baahubali
+director - Rajamouli
+producers - Sobhu, Raghavendra, Prasad
+lead actors with their roles - an object with the key-value pairs: prabhas - Baahubali, anushka - Devasena, rana - Bhallaladeva, tamannah - Avanthika
+Output
+
+The output should be a single line containing a movie object with the above properties.
+*/
+let object2 = {
+    title: 'Baahubali',
+    director: 'Rajamouli',
+    producers: ['Sobhu','Raghavendra','Prasad'],
+    'lead actors with their roles':{
+        prabhas:'Baahubali',
+        anushka:'Devasena',
+        rana:'Bhallaladeva',
+        tamannah:'Avanthika'
+    }
+};
+
+console.log(object2)
