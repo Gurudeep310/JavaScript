df873c72904feedd9cf014dc6b72758f5239616e
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:28:18 +0530

    Create 96_Create_and_log_array.js

---
+++ 96_Create_and_log_array.js
@@ -0,0 +1,10 @@
+/*
+Create & Log an Array
+Write a JS program to log an array with the given values.
+'Orange', 25, 100, true, 33.58.
+Output
+
+The output should be a single line containing an array with the above values in the same order.
+*/
+let array = ['Orange',25,100,true,33.58];
+console.log(array);
