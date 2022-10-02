4bc015d4df635cb0dba622c407714888f2a3afb5
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:18:26 +0530

    Create 100_add_value_to_array.js

---
+++ 100_add_value_to_array.js
@@ -0,0 +1,45 @@
+/*
+Add a value to the end of the array
+Given an array (myArray) and the value (val).
+Write a JS program to add a value to the end of the array using the array method push and log the updated array.
+Input
+
+The first line of input will contain an array (myArray) and the second line of input will contain an integer (val).
+Output
+
+The output should be a single line containing the updated array.
+*/
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  let input = readLine().replace(/'/g, '"');
+  // myArray
+  let myArray = JSON.parse(input);
+  // val
+  let val = parseInt(readLine());
+  myArray.push(val)
+  console.log(myArray)
+}
