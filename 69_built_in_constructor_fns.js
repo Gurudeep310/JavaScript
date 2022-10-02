2d4ffac7fc742ea34b1a0fdc834e836da5833726
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:26:09 +0530

    Create 69_built_in_constructor_fns.js

---
+++ 69_built_in_constructor_fns.js
@@ -0,0 +1,13 @@
+// Date constructor fn
+
+let now = new Date();
+console.log(now);
+console.log(typeof(now));
+
+let date = new Date("2021-02-02");
+console.log(date);
+
+let date1 = new Date(1947,0,15,1,3,15,0);
+console.log(date1);
+let date2 = new Date(1947,1,15);
+console.log(date2);
