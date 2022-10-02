82e944b3dd9ca92980eac1026ecb87079467f0a9
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:23:58 +0530

    Create 45_rest_sum.js

---
+++ 45_rest_sum.js
@@ -0,0 +1,12 @@
+function sum(...args)
+{
+    let result = 0;
+    for(let arg of args)
+    {
+        result = result + arg;
+    }
+    console.log(result);
+}
+
+sum(1,2,3);
+sum(1,2,3,4,5,6,7,8);
