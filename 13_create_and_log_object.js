c281bdb8e5407b7fea2fd83afa77498d1bb0bab3
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:21:08 +0530

    Create 13_create_and_log_object.js

---
+++ 13_create_and_log_object.js
@@ -0,0 +1,21 @@
+/*
+Create & Log an Object
+Write a JS program to log a vehicle object with the given properties (key-value pairs).
+
+ name - Mercedes Benz
+ model - C-Class
+ manufacturer - Daimler AG company
+ length - 4686mm
+ cargo capacity - 12.6 cubic feet
+Output
+
+The output should be a single line containing a vehicle object with the above properties.
+*/
+let object = {
+    name : 'Mercedes Benz',
+    model: 'C-Class',
+    manufacturer: 'Daimler AG company',
+    length: '4686mm',
+    'cargo capacity': '12.6 cubic feet'
+};
+console.log(object);
