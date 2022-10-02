502dc359f2cff448c0bd83b80310b5b6278cee25
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:26:16 +0530

    Create 70_date_fn_instance_methods.js

---
+++ 70_date_fn_instance_methods.js
@@ -0,0 +1,13 @@
+// Getting Date Values
+let date = new Date(1947,0,15,1,3,15,0);
+console.log(date);
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMonth());
+
+// Setting Date Values
+date.setFullYear(2021);
+date.setDate(1);
+console.log(date);
+
+
