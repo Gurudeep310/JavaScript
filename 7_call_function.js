b2b437fb57bea387e52bde7e84d675f719a9ed02
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:26:52 +0530

    Create 7_call_function.js

---
+++ 7_call_function.js
@@ -0,0 +1,13 @@
+/*
+Call a Function
+Given a function (greet) in the prefilled code.
+Write a JS code to call the given function and log it.
+Output
+
+The output should be a single line containing the given greetings message.
+*/
+function greet() {
+    return "Hello! Have a nice day";
+}
+  
+console.log(greet());
