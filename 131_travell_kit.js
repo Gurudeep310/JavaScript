fe4536283d00ca7f98e02fca6149a86cb60c2d97
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:20:24 +0530

    Create 131_travell_kit.js

---
+++ 131_travell_kit.js
@@ -0,0 +1,57 @@
+/*
+Given an object tavelBag in the prefilled code and item  as an input, write a JS program to add the method isKitContainsItem  to the constructor function Travel using the prototype
+The method isKitContainsItem checks whether the  travelBag contains the given item
+Sample Input 
+ticket
+Sample Output 
+true
+*/
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString.trim().split("\n").map((str) => str.trim());
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Please do not modify anything above this line */
+
+function Travel(kit, item) {
+  this.kit = kit;
+  this.item = item;
+}
+
+function main() {
+  const travelBag = {
+    ticket : "Hyderabad-Vizag",
+    clothes: ["Shirt", "T-Shirt","Jeans"],
+    medicines: "Paracetamol",
+    waterBottle: true,
+    snacks:true
+  };
+  
+  const item = readLine();
+  
+  const travelKit = Object.getOwnPropertyNames(travelBag);    
+  const travel = new Travel(travelKit, item);
+    Travel.prototype.isKitContainsItem = function(item){
+        return this.kit.includes(this.item);
+    };
+  console.log(travel.isKitContainsItem(item));
+}
+
+
