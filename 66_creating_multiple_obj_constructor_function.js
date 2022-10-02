b897f5374e0c177d38a7409176753fec47437aeb
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:25:59 +0530

    Create 66_creating_multiple_obj_constructor_function.js

---
+++ 66_creating_multiple_obj_constructor_function.js
@@ -0,0 +1,15 @@
+function Car(color,brand)
+{
+    this.color = color;
+    this.brand = brand;
+    this.start = function(){
+        console.log("Started");
+    };
+};
+
+let car1 = new Car("Blue","Audi");   // instance
+let car2 = new Car("Red","Tata");
+let car3 = new Car("Green","BMW");
+console.log(car1);
+console.log(car2);
+console.log(car3);
