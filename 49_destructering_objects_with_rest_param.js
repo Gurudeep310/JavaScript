8904007e2bc7aeaf40150eaefc8710cb847a336a
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:24:45 +0530

    Create 49_destructering_objects_with_rest_param.js

---
+++ 49_destructering_objects_with_rest_param.js
@@ -0,0 +1,10 @@
+let {firstName,...rest} = {
+    firstName: "Rahul",
+    lastName:"Rocks",
+    age: 22,
+};
+
+console.log(firstName);
+console.log(rest);    // Again rest is an object
+`Templeate Literals`
+`some text ${expression} some text`
