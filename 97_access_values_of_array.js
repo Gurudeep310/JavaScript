978a823354ef17d98cd9f39aaad01d87a6462a2c
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:28:25 +0530

    Create 97_access_values_of_array.js

---
+++ 97_access_values_of_array.js
@@ -0,0 +1,43 @@
+/*
+Access the values of an array
+Given an array (myArray) in the prefilled code.
+Write a JS program to log the value located at the given index (index).
+Input
+
+The first line of input will contain a whole number (index).
+Output
+
+The output should be a single line containing the value located at the given index of the array.
+*/
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // index
+  let index = parseInt(readLine());
+  // myArray
+  let myArray = [25, false, "green", "apple"];
+  console.log(myArray[index])
+}
