4c6b448d1af83cc2fc90bc84a5d9b69ea151d1cc
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 20:25:52 +0530

    Create 64_creating_multiple_obj_object_literals.js

---
+++ 64_creating_multiple_obj_object_literals.js
@@ -0,0 +1,28 @@
+let car1 = {
+    color: "Blue",
+    brand: "Audi",
+    start: function(){
+        console.log("started");
+    }
+};
+
+let car2 = {
+    color: "Red",
+    brand: "Tata",
+    start: function(){
+        console.log("started");
+    }
+};
+
+let car3 = {
+    color: "Green",
+    brand: "BMW",
+    start: function(){
+        console.log("started");
+    }
+};
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
+
